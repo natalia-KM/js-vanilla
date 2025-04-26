d6fb745198609b95793f9c42129a8aaf07eb50c7
Nat <natalia.km47@gmail.com>
2025-04-26 19:10:38 +0100

    l3

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const button = document.getElementById('my-button')
-const str = 'str'
+const myDiv = document.getElementById('my-div')
 
-button.addEventListener('click', () => {
-    alert('JS')
+button.addEventListener('mouseenter', () => {
+    myDiv.style.color = 'red'
+    myDiv.innerHTML = `
+    <div>
+        Mouse entered
+    </div>`
 })
+
+//  functions - keywords, indents
+//  variable scope - Set / UpdateContext
+//  switch / match
+//  lists - tables
+//  lambda - filter, maybe map?
+//  classes and objects
+//  enums
+
+//  exception handling?
+//  SQL?
